ab5234a301d21bd888d14f9461b2d431718fd83b
akbarmayat <77452182+akbarmayat@users.noreply.github.com>
2021-09-11 02:38:30 +0700

    Update rakyatsafelink.js

--- rakyatsafelink.js
+++ rakyatsafelink.js
@@ -129,7 +129,7 @@ j++;
 }
 if (a_to_vh == false) {
 var encryptedUrl = Base64.encode(a_to_vi);
-a_to_ve[i].href = "https://rakyatnesia.com/daftar-perusahaan-asuransi-terbaik-di-indonesia/?url=" + encryptedUrl;
+a_to_ve[i].href = "https://rakyatnesia.com/daftar-perusahaan-asuransi-terbaik-di-indonesia/" + encryptedUrl;
 a_to_ve[i].rel = "nofollow";
 a_to_vb++;
 a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
